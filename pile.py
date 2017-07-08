e60ecb7bf272fd02e70879102693ac622136318e
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2017-07-08 18:58:31 +0000

    Lint fixes, Unicode fixes. Add ID field

--- pile.py
+++ pile.py
@@ -1,15 +1,22 @@
-import sys
+# coding: utf-8
+
 import re
 from pathlib import Path
-import json
 import os
 import os.path
-import click
 
-re_date = re.compile("(\d\d\d\d-\d\d-\d\d)(.*)")
-re_ext = re.compile(".*?[.]([a-zA-Z]{1,3})")
-re_kvtag = re.compile("[#]([A-Za-z0-9]+)=([A-Za-z0-9]+)")
-re_tag = re.compile("[#][A-Za-z0-9]+")
+
+# MacOSX insists in filenames being utf8, with NFD normalized
+# This means umlauts are represented as two unicode points : (1) LATIN SMALL LETTER ? (2) WITH COMBINING DIAERESIS
+# We explicitly include the latter unicode point
+# See, e.g. https://tex.stackexchange.com/questions/94418/os-x-umlauts-in-utf8-nfd-yield-package-inputenc-error-unicode-char-u8̈-not
+
+W = u'[A-Za-z0-9_\u0308\u006B]'
+RE_ID = re.compile(u'[#]([0-9])')
+RE_DATE = re.compile(u"(\d\d\d\d-\d\d-\d\d)(.*)")
+RE_EXT = re.compile(u".*?[.]([a-zA-Z]{1,3})")
+RE_KVTAG = re.compile(u"[#]({w}+)=({w}+)".format(w=W))
+RE_TAG = re.compile(u'[#]{w}+'.format(w=W))
 
 def tag2str(tag):
     return '#' + tag
@@ -17,19 +24,31 @@ def tag2str(tag):
 def str2tag(s):
     return s.lstrip("#")
 
-def kvtag2str(k,v):
+def kvtag2str(k, v):
     return "#" + k + "=" + v
 
 class Document:
     "A managed document"
-    
-    def __init__(self, date, tags, kvtags, title, ext, path):
+
+    def __init__(self, id, date, tags, kvtags, title, ext, path):
+        self.id = id
         self.date = date
         self.tags = tags
         self.kvtags = kvtags
         self.ext = ext
         self.title = title
         self.path = path
+
+    def todict(self):
+        return {
+            "id" : self.id,
+            "date" : self.date,
+            "tags" : self.tag_list(),
+            "kvtags" : self.kvtags,
+            "title" : self.title,
+            "extension" : self.ext,
+            "filename" : self.path.name
+        }
 
     @staticmethod
     def from_path(path):
@@ -40,11 +59,18 @@ class Document:
         # http://beets.io/blog/paths.html
         name = path.name
 
+        match = RE_ID.search(name)
+        if match:
+            id = match.group(1)
+            name = RE_ID.sub("", name)
+        else:
+            raise ValueError("No ID found in '{}'".format(name))
+
         # Split off extension
         base, ext = os.path.splitext(name)
 
         # Split off date
-        match = re_date.match(base)
+        match = RE_DATE.match(base)
         rest = None
         date = None
         if match:
@@ -59,26 +85,26 @@ class Document:
             raise ValueError("Path not a file")
 
         # Extract kv-tags
-        kvtags = dict(re_kvtag.findall(rest))
-        rest = re_kvtag.sub("", rest)
+        kvtags = dict(RE_KVTAG.findall(rest))
+        rest = RE_KVTAG.sub("", rest)
 
         # Extract tags
-        tags = [ str2tag(tag_str) for tag_str in re_tag.findall(rest) ]
+        tags = [str2tag(tag_str) for tag_str in RE_TAG.findall(rest)]
         tags = list(set(tags))
         tags.sort()
-        rest = re_tag.sub("", rest)
+        rest = RE_TAG.sub("", rest)
 
         # Fetch remaining bits as title
         rest = re.sub("[_,|:]", " ", rest)
         rest = re.sub("  +", " ", rest)
         rest = rest.strip(" ")
         title = rest
-        return Document(date, tags, kvtags, title, ext, path)
-        
+        return Document(id, date, tags, kvtags, title, ext, path)
+
     def text(self):
-        tags = "".join([ tag2str(t) + " " for t in self.tags ])
-        kvtags = "".join([ kvtag2str(k,v) + " " for k,v in self.kvtags.items()])
-        return '{} {}{}{}{}'.format(self.date, tags, kvtags, self.title, self.ext)
+        tags = "".join([tag2str(t) + " " for t in self.tags])
+        kvtags = "".join([kvtag2str(k, v) + " " for k, v in self.kvtags.items()])
+        return '{} #{} {}{}{}{}'.format(self.date, self.id, tags, kvtags, self.title, self.ext)
 
     def normalize(self):
         q = self.path.with_name(self.text())
@@ -102,14 +128,14 @@ class Document:
     def tag_add(self, tag):
         "Add tag to tags list"
         if not tag in self.tags:
-            self.tags += [ tag ]
+            self.tags += [tag]
             self.tags.sort()
             self.normalize()
-            
+
     def tag_rm(self, tag):
         "remove tag from internal tag list. Need to normalize() to change file name"
         if tag in self.tags:
-            self.tags = [ t for t in self.tags if t != tag ]
+            self.tags = [t for t in self.tags if t != tag]
             self.normalize()
 
 class Pile():
@@ -118,7 +144,7 @@ class Pile():
     def __init__(self, backing_dir):
         self.docs = []
         self.backing_dir = Path(backing_dir)
-        assert(self.backing_dir.is_dir())
+        assert self.backing_dir.is_dir()
 
     def __iter__(self):
         "iterate over documents within the pile"
@@ -135,7 +161,7 @@ class Pile():
             try:
                 pile.add(Document.from_path(p))
             except ValueError as e:
-                pass
+                print(e)
         return pile
 
     @staticmethod
